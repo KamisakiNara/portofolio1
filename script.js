cd2ace34fa12410195a7f4f8301e8075bf9d1c05
KamisakiNara <muhamadraihan2468@gmail.com>
2025-09-29 21:05:02 +0700

    update

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 // Dark mode toggle
 const toggle = document.getElementById("darkModeToggle");
 toggle.addEventListener("change", function () {
   document.body.classList.toggle("dark-mode", this.checked);
 });
-=======
-// Dark mode toggle
-const toggle = document.getElementById("darkModeToggle");
-toggle.addEventListener("change", function () {
-  document.body.classList.toggle("dark-mode", this.checked);
-});
->>>>>>> 8549e14974f1f52bb61302ecf3b10f997af46579
